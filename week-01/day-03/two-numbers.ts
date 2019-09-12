8a6139876666454add0eea7bf6d807a008dbc561
orsilarssen <orsilarssen@gmail.com>
2019-09-12 16:15:40 +0200

    two-numbers.ts

---
+++ week-01/day-03/two-numbers.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+console.log(13 + 22);
+
+console.log(22 - 13);
+
+console.log(22 * 13);
+
+console.log(22 / 13);
+
+console.log(22 % 13);
