f18897cf7c6b9d50015904b4e4fc368fb3b89e36
orsilarssen <orsilarssen@gmail.com>
2019-10-02 10:48:43 +0200

    day 2 exercises

--- week-03/day-05/sharpie-set.ts
+++ week-03/day-05/sharpie-set.ts
@@ -1,6 +1,7 @@
 /*
 Create Sharpie class
-We should know about each sharpie their color (which should be a string), width (which will be a floating point number), inkAmount (another floating point number)
+We should know about each sharpie their color (which should be a string), 
+width (which will be a floating point number), inkAmount (another floating point number)
 When creating one, we need to specify the color and the width
 Every sharpie is created with a default 100 as inkAmount
 We can use() the sharpie objects which decreases inkAmount
@@ -14,11 +15,12 @@ removeTrash() -> removes all unusable sharpies*/
 class sharpie {
     private _color: string;
     private _width: number;
-    private _inkAmount: number = 100;
+    private _inkAmount: number;
 
-    constructor(color: string, width: number) {
+    constructor(color: string, width: number, inkamount: number = 100) {
         this._color = color;
         this._width = width;
+        this._inkAmount=inkamount;
     }
 
     public use(): void {
@@ -29,7 +31,12 @@ class sharpie {
 let pens = new sharpie('red', 0.56);
 pens.use();
 console.log(pens)
+
+
+
 class sharpieSet {
+    listOfSharpie: string;
+    
 
 
 }
