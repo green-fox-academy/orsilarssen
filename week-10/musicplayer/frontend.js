4ca249b176ead77e10e42065d66cc6d6458c680d
orsilarssen <orsilarssen@gmail.com>
2019-11-25 17:43:06 +0100

    html, css and frontend ready

--- week-10/musicplayer/frontend.js
+++ week-10/musicplayer/frontend.js
@@ -1,1 +1,16 @@
 //frontend
+
+let express = require('express');
+let app = express();
+var fetch = require('node-fetch')
+
+app.use(express.static('static'))
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname+ '/main.html');
+});
+
+app.listen(8080, function () {
+  console.log('Node app is running on port 8080');
+});
+
